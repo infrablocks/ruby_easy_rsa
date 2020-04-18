97d53340ab153698fa99667ccdaefa2fe70f4b6d
Toby Clemson <tobyclemson@gmail.com>
2020-04-18 22:46:09 +0100

    Add global config to InitialisePKI command.

--- lib/ruby_easy_rsa/commands/initialise_pki.rb
+++ lib/ruby_easy_rsa/commands/initialise_pki.rb
@@ -5,9 +5,23 @@ module RubyEasyRSA
   module Commands
     class InitialisePKI < Base
       def configure_command(builder, opts)
+        openssl_binary = opts[:openssl_binary]
+        ssl_configuration = opts[:ssl_configuration]
+        safe_configuration = opts[:safe_configuration]
+        extensions_directory = opts[:extensions_directory]
+        vars = opts[:vars]
         pki_directory = opts[:pki_directory]
 
-        builder = builder.with_option('--pki-dir', pki_directory) if pki_directory
+        builder = builder.with_environment_variable(
+            'EASYRSA_OPENSSL', openssl_binary) if openssl_binary
+        builder = builder.with_environment_variable(
+            'EASYRSA_SSL_CONF', ssl_configuration) if ssl_configuration
+        builder = builder.with_environment_variable(
+            'EASYRSA_SAFE_CONF', safe_configuration) if safe_configuration
+        builder = builder.with_option(
+            '--vars', vars) if vars
+        builder = builder.with_option(
+            '--pki-dir', pki_directory) if pki_directory
         builder.with_subcommand('init-pki')
       end
     end
--- spec/commands/initialise_pki_spec.rb
+++ spec/commands/initialise_pki_spec.rb
@@ -44,4 +44,63 @@ describe RubyEasyRSA::Commands::InitialisePKI do
     command.execute(
         pki_directory: pki_directory)
   end
+
+  it 'uses the provided openssl binary when specified' do
+    openssl_binary = "./vendor/openssl/bin/openssl"
+
+    command = RubyEasyRSA::Commands::InitialisePKI.new
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with('EASYRSA_OPENSSL="./vendor/openssl/bin/openssl" ' +
+                'path/to/binary init-pki',
+                any_args))
+
+    command.execute(
+        openssl_binary: openssl_binary)
+  end
+
+  it 'uses the provided ssl configuration when specified' do
+    ssl_configuration = "./vendor/easyrsa/openssl-easyrsa.cnf"
+
+    command = RubyEasyRSA::Commands::InitialisePKI.new
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with('EASYRSA_SSL_CONF="./vendor/easyrsa/openssl-easyrsa.cnf" ' +
+                'path/to/binary init-pki',
+                any_args))
+
+    command.execute(
+        ssl_configuration: ssl_configuration)
+  end
+
+  it 'uses the provided safe configuration when specified' do
+    safe_configuration = "./vendor/easyrsa/safessl-easyrsa.cnf"
+
+    command = RubyEasyRSA::Commands::InitialisePKI.new
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with('EASYRSA_SAFE_CONF="./vendor/easyrsa/safessl-easyrsa.cnf" ' +
+                'path/to/binary init-pki',
+                any_args))
+
+    command.execute(
+        safe_configuration: safe_configuration)
+  end
+
+  it 'uses the provided vars when specified' do
+    vars = "./pki/vars"
+
+    command = RubyEasyRSA::Commands::InitialisePKI.new
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with('path/to/binary --vars=./pki/vars init-pki',
+                any_args))
+
+    command.execute(
+        vars: vars)
+  end
 end
