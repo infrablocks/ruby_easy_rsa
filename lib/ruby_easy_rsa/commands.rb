4c7643ef511653fbfcd57707d6c029da8efe37eb
Toby Clemson <tobyclemson@gmail.com>
2020-04-19 19:23:07 +0100

    Add UpdateDB command.

--- lib/ruby_easy_rsa/commands.rb
+++ lib/ruby_easy_rsa/commands.rb
@@ -7,6 +7,7 @@ require_relative 'commands/build_client_full'
 require_relative 'commands/build_server_full'
 require_relative 'commands/revoke'
 require_relative 'commands/gen_crl'
+require_relative 'commands/update_db'
 
 module RubyEasyRSA
   module Commands
---
+++ lib/ruby_easy_rsa/commands/update_db.rb
@@ -0,0 +1,20 @@
+require 'lino'
+
+require_relative 'base'
+require_relative 'mixins/global_config'
+require_relative 'mixins/ssl_config'
+
+module RubyEasyRSA
+  module Commands
+    class UpdateDB < Base
+      include Mixins::GlobalConfig
+      include Mixins::SSLConfig
+
+      def configure_command(builder, opts)
+        builder = builder.with_subcommand('update-db')
+        builder = super(builder, opts)
+        builder
+      end
+    end
+  end
+end
