0f6921e98d7fb24ad8464763aab1b9d146783de3
Toby Clemson <tobyclemson@gmail.com>
2020-04-21 11:42:42 +0100

    Add Renew command.

---
+++ lib/ruby_easy_rsa/commands/renew.rb
@@ -0,0 +1,25 @@
+require 'lino'
+
+require_relative 'base'
+require_relative 'mixins/global_config'
+require_relative 'mixins/ssl_config'
+require_relative 'mixins/encrypt_key_config'
+
+module RubyEasyRSA
+  module Commands
+    class Renew < Base
+      include Mixins::GlobalConfig
+      include Mixins::SSLConfig
+      include Mixins::EncryptKeyConfig
+
+      def configure_command(builder, opts)
+        filename_base = opts[:filename_base]
+
+        builder = builder.with_subcommand('renew')
+        builder = builder.with_argument(filename_base)
+        builder = super(builder, opts)
+        builder
+      end
+    end
+  end
+end
